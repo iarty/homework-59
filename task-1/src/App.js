4555ab8d0855e4ad6567afcf8fb806e190df5e7a
Barbonov Artem <abarbonov@gmail.com>
2019-12-04 10:54:01 +0600

    created components and container in task 1

---
+++ task-1/src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import FilmWatchList from './Container/FilmWatchList'
+
+export default function App() {
+  return (
+    <Fragment>
+      <FilmWatchList />
+    </Fragment>
+  )
+}
